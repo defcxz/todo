b5d1302bdcfaf456e9ff96eddfe78a2c1a5f1563
Mario Gómez <ma.gomez@duocuc.cl>
2023-12-17 17:41:51 -0300

    Mejoras varias

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,50 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Input, Button, Checkbox } from '@nextui-org/react'
-// TODO: Let table be a component
+import { Input, Button } from '@nextui-org/react'
+// TODO: Let table to be a component
 import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/react'
+import { DeleteIcon } from './components/DeleteIcon'
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('')
-  const [items, setItems] = useState<string[]>([])
+  const [items, setItems] = useState<{ name: string, done: boolean }[]>([])
 
   useEffect(() => {
     let storedItems = []
 
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i) || ''
-      const value = localStorage.getItem(key)
+      const value = JSON.parse(localStorage.getItem(key) || '')
       storedItems.push(value)
     }
-    setItems(storedItems.filter(item => item !== null) as string[])
+    return setItems(storedItems.filter(item => item !== null) as { name: string, done: boolean }[])
   }, []);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    localStorage.setItem(inputValue, inputValue)
-    const newItems = [...items, inputValue]
+    const newItem = { name: inputValue, done: false }
+    localStorage.setItem(inputValue, JSON.stringify(newItem))
+    const newItems = [...items, newItem]
     setItems(newItems)
     setInputValue('')
   }
 
   const handleDelete = (item: string) => {
     localStorage.removeItem(item)
-    const newItems = items.filter(i => i !== item)
+    const newItems = items.filter(i => i.name !== item)
     setItems(newItems)
   }
 
+  const handleCheck = (item: string) => {
+    const newItems = items.map(i => i.name === item ? { ...i, done: !i.done } : i)
+    localStorage.setItem(item, JSON.stringify(newItems.find(i => i.name === item)))
+    setItems(newItems)
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-5">
-      <form className='flex gap-2 items-center' onSubmit={handleSubmit}>
+    <main className="flex min-h-screen flex-col w-5/6 md:w-1/2 mx-auto items-center pt-5">
+      <form className='flex gap-2 items-center justify-around' onSubmit={handleSubmit}>
         <Input
           id="input"
           isClearable
@@ -53,43 +60,46 @@ export default function Home() {
           onClear={() => {
             setInputValue('')
           }}
-          className="md:w-96"
+          className="md:w-[40vw]"
         />
-        <Button
-          size='lg'
-        >
+        <Button size='lg'>
           Add
         </Button>
       </form>
-        <Table
-          aria-label='to-do table'
-          defaultSelectedKeys={["0"]}
-          className={'mt-8 md:w-96'}
-        >
-          <TableHeader>
-            <TableColumn>To-do</TableColumn>
-            <TableColumn width={1}>Done</TableColumn>
-          </TableHeader>
-          <TableBody emptyContent={"No tasks to display."}>
-            {
-              items.map((item, index) => (
-                <TableRow key={index}>
-                  <TableCell>{item}</TableCell>
-                  <TableCell>
-                    <Checkbox
-                      id={`checkbox-${index}`}
-                      onValueChange={() => {
-                        setTimeout(() => {
-                          handleDelete(item)
-                        }, 1000)
-                      }}
-                    />
-                  </TableCell>
-                </TableRow>
-              ))
-            }
-          </TableBody>
-        </Table>
+      <Table
+        aria-label='to-do table'
+        className={'mt-8'}
+        selectionMode='multiple'
+      >
+        <TableHeader>
+          <TableColumn>To-do</TableColumn>
+          <TableColumn width={1}>Delete</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No tasks to display."}>
+          {
+            items.map((item, index) => (
+              <TableRow
+                key={index}
+                onClick={() => {
+                  handleCheck(item.name)}
+                }
+              >
+                <TableCell>{item.name}</TableCell>
+                <TableCell>
+                  <Button
+                    variant='ghost'
+                    isIconOnly
+                    aria-label="Delete task"
+                    onClick={() => handleDelete(item.name)}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
     </main>
   )
 }
